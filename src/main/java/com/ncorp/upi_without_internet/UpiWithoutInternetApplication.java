9c515305adc0d81efed45076553ab3731b9a6721
Naman Sharma <naman.sharma.122003@gmail.com>
2026-06-30 21:10:23 +0530

    cfg: Added application properties

--- src/main/java/com/ncorp/upi_without_internet/UpiWithoutInternetApplication.java
+++ src/main/java/com/ncorp/upi_without_internet/UpiWithoutInternetApplication.java
@@ -2,8 +2,10 @@ package com.ncorp.upi_without_internet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class UpiWithoutInternetApplication {
 
 	public static void main(String[] args) {
